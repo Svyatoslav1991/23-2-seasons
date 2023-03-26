484a75c30f52d7062ff82572caa21ff81f2f4587
Svyatoslav Myshkovskiy <svatoslav190391@yandex.ru>
2023-03-26 17:47:14 +0300

    initial

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+//#define SPRING 1
+//#define SUMMER 2
+#define AUTUMN 3
+//#define WINTER 4
+
+int main()
+{
+	std::cout << "\tTask 23.2 SEASONS\n\n";
+
+#if SPRING
+	std::cout << "Spring\n";
+#elif SUMMER
+	std::cout << "Summer\n";
+#elif AUTUMN
+	std::cout << "Autumn\n";
+#elif WINTER
+	std::cout << "Winter\n";
+#endif
+
+
+	return 0;
+}
